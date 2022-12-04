e3c021ebf7cac7b5b20a02abac13e43206d738e6
Danilo Mera <danilo@Danilos-Mac-mini.local>
2022-12-03 19:00:36 -0700

    setting up location and weather adon for navigation

---
+++ MySchoolSystem/Controllers/AdonsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MySchoolSystem.Controllers
+{
+    public class AdonsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Weather(int latitude, int longitude)
+        {
+            //https://api.open-meteo.com/v1/forecast?latitude=33.44&longitude=-112.07&hourly=temperature_2m&temperature_unit=fahrenheit
+            return View();
+        }
+    }
+}
---
+++ MySchoolSystem/wwwroot/js/geolocation.js
@@ -0,0 +1,17 @@
+﻿GetLocation = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+        position => { resolve([position.coords.latitude, position.coords.longitude]) },
+        error => { reject(error) }
+    )
+})
+function GetWeather() {
+    let baseURL = "https://api.open-meteo.com/v1/forecast";
+    GetLocation.then(([lat, long], rej) => { 
+        fetch(`${baseURL}?latitude=${lat}&longitude=${long}&hourly=temperature_2m&temperature_unit=fahrenheit`)
+            .then(res => res.json())
+            .then(function (res) {
+                console.log(res)
+                return res.hourly["temperature_2m"][0]
+            })
+    })
+}
